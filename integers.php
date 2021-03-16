36af04378a863d899e26dcd6a9539d911e5c276d
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-16 11:53:31 -0400

    learning about numbers

---
+++ integers.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Strings</title>
+    </head>
+<body>
+    <?php
+        $var1 = 3;
+        $var2 +=3; //shorthand increment can be done with all operators
+    ?>
+</body>
+</html>
