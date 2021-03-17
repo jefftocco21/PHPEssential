0342cc660dd7672515a0e8142c268a9e5633648b
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-03-17 08:32:08 -0400

    switch statement practice

---
+++ switch.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Floating Points Numbers</title>
+    </head>
+<body>
+    <?php
+        $a=3;
+    
+        switch($a){
+            case 0:
+                echo "a equals 0<br/>";
+                break;
+            case 1:
+                echo "a equals 1<br/>";
+                break;
+            case 2:
+                echo "a equals 2<br/>";
+                break;
+            case 3:
+                echo "a equals 3<br/>";
+                break;         
+            }
+        
+
+
+    ?>
+</body>
+</html>
